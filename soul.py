ec2c06d136e0e85766cfce2d82ad47c57455d14a
Salmanrokkie07 <100474219+Salmanrokkie07@users.noreply.github.com>
2024-07-31 22:32:17 +0530

    Add files via upload

--- soul.py
+++ soul.py
@@ -16,7 +16,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = 7430083280:AAEiaz1IAmtvjkBOzMObuzz7FCpG8qV-0zI
+TOKEN = 7251028926:AAGs2QSJge7iww6pEQNFoIHzIb_b1Ymf2-I
 MONGO_URI = 'mongodb+srv://piroop:piroop@piro.hexrg9w.mongodb.net/?retryWrites=true&w=majority&appName=piro&tlsAllowInvalidCertificates=true'
 FORWARD_CHANNEL_ID = -100887532328
 CHANNEL_ID = -100887532328
